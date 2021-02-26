e651a63e72d25b42a417ea234d3595c218df5119
Jonatan Koroch de Aguiar <jonatanaguiar1998@hotmail.com>
2021-02-26 03:04:46 -0300

    Dia 4: Trabalhando com emails

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,14 +1,18 @@
 import { Router }  from "express";
 import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from "./controllers/UserController";
+import { SendEmailController } from "./controllers/SendEmailController";
 
 const router = Router();
 const userController = new UserController();
 const surveyController = new SurveysController();
+const sendEmailController = new SendEmailController();
 
 router.post("/users", userController.create);
 
 router.get("/surveys", surveyController.show);
 router.post("/surveys", surveyController.create);
+
+router.post("/sendEmail", sendEmailController.execute);
 
 export { router };
